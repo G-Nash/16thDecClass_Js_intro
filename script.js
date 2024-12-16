97f349edc62b802b4c532e4bbd2021002cf08af3
G-Nash <20b91a5725@srkrec.ac.in>
2024-12-16 10:26:05 +0530

    content

--- script.js
+++ script.js
@@ -2,3 +2,24 @@ console.log("typing...")
 
 console.log(69 + 69)
 
+var a 
+a =110
+console.log(a)
+
+
+var b=20
+console.log(b)
+
+var x
+
+x="this is a string"
+console.log(x)
+
+console.log(typeof b)
+console.log(typeof x)
+
+console.log(100 * "nash")
+console.log(100 * "100")
+
+console.log('g' + '_' + 'nash')
+
